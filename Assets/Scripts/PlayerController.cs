0a4d0b21014478b6aa0bef8a9f47b4ba385438ab
Eduardo <educoloni12@gmail.com>
2024-04-06 11:54:07 -0300

    Sistema do player iniciado

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Pegando as informações do player/Criando as varáiveis
+    private Rigidbody2D meuRB;
+
+    //Minha velocidade
+    [SerializeField] private float velocidade = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meuRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Subir();
+        LimitandoVelocidade();
+    }
+
+    private void LimitandoVelocidade()
+    {
+        //Limitando a minha velocidade de queda
+        if (meuRB.velocity.y < -velocidade)
+        {
+            //Travando meuRB.velocity em -5
+            meuRB.velocity = Vector2.down * velocidade;
+        }
+    }
+
+    private void Subir()
+    {
+        //Subindo ao apertar espaço
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Fazendo a velocidade do RB ir para cima
+            meuRB.velocity = Vector2.up * velocidade;
+        }
+    }
+}
